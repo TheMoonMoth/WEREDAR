a51704c5b4ea018884b1722377b09be6d1168436
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-12-20 11:38:35 -0700

    changes

--- map.js
+++ map.js
@@ -1,3 +1,7 @@
+const wolfApi = ""
+const locationsApi = ""
+const updates = ""
+
 var mymap = L.map('mapid').setView([39.737818, -104.990117], 15)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -7,6 +11,9 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoidGhlbW9vbm1vdGgiLCJhIjoiY2piMmZ6b2l4MmFkdDJ3cWt0b3EwNjA1eCJ9.oS0Sc7_t4VAP5dKrQp9njw'
 }).addTo(mymap);
 
-var marker = L.marker([39.737818, -104.990017]).addTo(mymap);
+fetch()
 
-marker.bindPopup("<b>Don't get caught unaware!").openPopup()
+
+
+var marker1 = L.marker([39.737818, -104.990017]).addTo(mymap)
+marker1.bindPopup("<b>Don't get caught unaware!").openPopup()
