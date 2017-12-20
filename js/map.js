9d4f599803f8a597fa0126ad68d3a9fcdfac7112
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-12-20 13:17:40 -0700

    map getting more functional

--- js/map.js
+++ js/map.js
@@ -1,8 +1,8 @@
-const wolfApi = ""
-const locationsApi = ""
-const updates = ""
+const wolvesApi = "http://werewolf-tracker.herokuapp.com/wolves"
+const sightingsApi = "http://werewolf-tracker.herokuapp.com/sightings"
+const userSightingApi = "http://werewolf-tracker.herokuapp.com/userSighting"
 
-var mymap = L.map('mapid').setView([39.737818, -104.990117], 15)
+var mymap = L.map('mapid').setView([39.737818, -104.990117], 9)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -11,9 +11,30 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoidGhlbW9vbm1vdGgiLCJhIjoiY2piMmZ6b2l4MmFkdDJ3cWt0b3EwNjA1eCJ9.oS0Sc7_t4VAP5dKrQp9njw'
 }).addTo(mymap);
 
-fetch()
+
+let wolves = []
+let sightings = []
+
+fetch(sightingsApi)
+  .then(response => response.json())
+  .then(response => sightings = response)
+
+fetch(wolvesApi)
+  .then(response => response.json())
+  .then(response => wolves = response)
 
 
+function pinWolf(sightingArray, wolvesArray){
+  let names = []
+  for (var i = 0 ; i < wolvesArray.length; i++){
+    names.push(wolves[i].name)
+  }
+  for (var j = 0; j < sightingArray.length; j++){
+    let marker = L.marker(sightingArray[j].locations[0]).addTo(mymap)
+    marker.bindPopup(`The ${wolvesArray[j].color} wolf named ${wolvesArray[j].name} was last seen here`).openPopup()
+  }
+}
 
-var marker1 = L.marker([39.737818, -104.990017]).addTo(mymap)
-marker1.bindPopup("<b>Don't get caught unaware!").openPopup()
+setTimeout(function(){
+  pinWolf(sightings, wolves)
+}, 1000)
