c4276d132d0da972bd9d7d6e80f31f3f2763c46b
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2017-12-20 15:29:48 -0700

    bugs

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
-const wolvesApi = "http://werewolf-tracker.herokuapp.com/wolves"
-const sightingsApi = "http://werewolf-tracker.herokuapp.com/sightings"
-const userSightingApi = "http://werewolf-tracker.herokuapp.com/userSighting"
+const wolvesApi = "https://werewolf-tracker.herokuapp.com/wolves"
+const sightingsApi = "https://werewolf-tracker.herokuapp.com/sightings"
+const userSightingApi = "https://werewolf-tracker.herokuapp.com/userSighting"
 
 var mymap = L.map('mapid').setView([39.737818, -104.990117], 9)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoidGhlbW9vbm1vdGgiLCJhIjoiY2piMmZ6b2l4MmFkdDJ3cWt0b3EwNjA1eCJ9.oS0Sc7_t4VAP5dKrQp9njw'
