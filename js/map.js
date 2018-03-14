25ca478ba5761a48121fec01a018659b79bb69a5
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-14 11:07:59 -0600

    Kyle found the fix

--- js/map.js
+++ js/map.js
@@ -10,9 +10,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoidGhlbW9vbm1vdGgiLCJhIjoiY2piMmZ6b2l4MmFkdDJ3cWt0b3EwNjA1eCJ9.oS0Sc7_t4VAP5dKrQp9njw'
 }).addTo(mymap);
-
-
-
+var markerGroup = L.layerGroup().addTo(mymap)
+var circleGroup = L.layerGroup().addTo(mymap)
 
 let wolves = []
 let sightings = []
@@ -52,8 +51,8 @@ getWolfSightings()
 
 
 function pinWolf(sightingArray, wolvesArray){
-  var markerGroup = L.layerGroup().addTo(mymap)
   markerGroup.clearLayers()
+  circleGroup.clearLayers()
   for (var j = 0; j < sightingArray.length; j++){
     let marker = L.marker(locations[sightingArray[j].locations[0]]).addTo(markerGroup)
     marker.bindPopup(`The ${wolvesArray[j].color} wolf named ${wolvesArray[j].name} was last seen in ${sightingArray[j].locations[0]}`).openPopup()
@@ -62,7 +61,7 @@ function pinWolf(sightingArray, wolvesArray){
       fillColor: '#823129',
       fillOpacity: 0.5,
       radius: 1000
-    }).addTo(mymap)
+    }).addTo(circleGroup)
   }
 }
 
